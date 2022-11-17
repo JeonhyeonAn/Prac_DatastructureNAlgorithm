dbf43c21281f20dfb6fae94895ff572325ecbd29
JeonhyeonAn <ajh7124@gmail.com>
2022-11-18 00:24:51 +0900

    .

--- 1.동적크기배열구현/1.동적크기배열구현.cpp
+++ 1.동적크기배열구현/1.동적크기배열구현.cpp
@@ -34,7 +34,7 @@ public:
 		this->n = n;
 		data = new T[n];
 	}
-	
+
 
 	dynamic_array(const dynamic_array<T>& other)
 	{
@@ -49,7 +49,61 @@ public:
 	{
 		return data[index];
 	}
+
+
+	T& at(int index)
+	{
+		if (index < n)
+			return data[index];
+		throw "Index out of range";
+	}
+
+
+
+
+	size_t size() const
+	{
+		return n;
+	}
+
+	// 소멸자
+	~dynamic_array()
+	{
+		delete[] data;	// 메모리 릭 방지
+	}
+
+
+	T* begin() { return data; }
+	const T* begin() const { return data; }
+	T* end() { return data + n; }
+	const T* end() const { return data + n; }
+
+	// + 연산   다시
+	friend dynamic_array<T> operator+(const dynamic_array<T>& arr1, dynamic_array<T>& arr2)
+	{
+		dynamic_array<T> result(arr1.size() + arr2.size_t());
+		std::copy(arr1.begin(), arr1.end(), result.begin());
+		std::copy(arr2.begin(), arr2.end(), result.begin() + arr1.size());
+	
+		return result;
+	}
+
+
+	std::string to_string(const std::string& sep = ", ")
+	{
+		if (n == 0)
+			return "";
+
+		std::ostringstream os;
+		os << data[0];
+
+		for (int i = 1; i < n; i++)
+			os << sep << data[i];
+
+		return os.str();
+	}
 };
+
 
 
 
