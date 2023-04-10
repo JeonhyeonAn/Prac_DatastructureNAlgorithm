43b67a51fff16b0d1ab9d89c548f376f08342cfc
ajh7124 <86709249+JeonhyeonAn@users.noreply.github.com>
2023-04-10 19:04:00 +0900

    .

--- 실습 01.음악 재생 목록 구현하기/실습 01.음악 재생 목록 구현하기.cpp
+++ 실습 01.음악 재생 목록 구현하기/실습 01.음악 재생 목록 구현하기.cpp
@@ -108,10 +108,64 @@ public:
 		}
 
 
-		// 후행 연산자
+		// ++ 연산자 (선행, 후행)
 		cir_list_it& operator++()
 		{
-			pt
+			ptr = ptr->next;
+			return *this;
+		}
+
+		cir_list_it operator++(int)
+		{
+			cir_list_it it = *this;
+			++(*this);
+			return it;
+		}
+
+		// -- 연산자
+		cir_list_it& operator--()
+		{
+			ptr = ptr->prev;
+			return *this;
+		}
+
+		cir_list_it operator--(int)
+		{
+			cir_list_it it = *this;
+			--(*this);
+			return it;
+		}
+					
+		// 반복자 비교 연산자 
+		friend bool operator==(const cir_list_it& it1, const cir_list_it& i + 2)
+		{
+			return it1.ptr == it2.ptr;
+		}
+		friend bool operator!=(const cir_list_it& it1, const cir_list_it& i + 2)
+		{
+			return it1.ptr != it2.ptr;
+		}
+
+
+		// begin(), end() 
+		cir_list_it begin()
+		{
+			return cir_list_it {head};
+		}
+
+		cir_list_it begin() const 
+		{
+			return ctir_list_it {head};
+		}
+
+		cir_list_it end()
+		{
+			return cir_list_it{ head->prev };
+		}
+
+		cir_list_it end() const
+		{
+			return cir_list_it { head->prev };
 		}
 	};
 };
